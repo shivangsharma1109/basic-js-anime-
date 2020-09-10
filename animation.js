6f0ebee526b1e572c6f6680578930c92922b535f
shivangsharma1109 <shivang1931076@akgec.ac.in>
2020-09-09 17:09:01 -0700

    updated the animation

--- animation.js
+++ animation.js
@@ -31,32 +31,63 @@ var cntxt = canvas.getContext('2d');
 // cntxt.strokeStyle = 'blue';
 // cntxt.stroke();
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = 4;
-var dy = 4;
-var radius = 40;
+
+function Circle(x, y, dx, dy, radius){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius=radius;
+
+    this.draw = function(){
+        cntxt.beginPath();
+        cntxt.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    
+        cntxt.strokeStyle = 'black';
+        cntxt.stroke();
+        cntxt.fillStyle = "pink";
+        cntxt.fill();
+       
+    }
+
+    this.update = function(){
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+           this.dx=-this.dx;
+        }
+        if(this.y + this.radius > innerWidth || this.y - this.radius < 0){
+            this.dy = -this.dy;
+        }
+    
+       this.x+=this.dx;
+       this.y+=this.dy;
+
+       this.draw();
+
+    }
+}
+
+
+var circleArray = [];
+for(var i=0; i<100; i++){
+ var x = Math.random() * (innerWidth - radius * 2) + radius;
+ var y = Math.random() * (innerHeight - radius * 2) + radius;
+ var dx = (Math.random() - 0.5) * 8;
+ var dy = (Math.random() - 0.5) * 8;;
+ var radius = 30;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+   
+}
+
+
+
+
+
 function animate() {
     requestAnimationFrame(animate);
     cntxt.clearRect(0, 0, innerWidth, innerHeight);
-    cntxt.beginPath();
-    cntxt.arc(x, y, radius, 0, Math.PI * 2, false);
-
-    cntxt.strokeStyle = 'black';
-    cntxt.stroke();
-    cntxt.fillStyle = "pink";
-    cntxt.fill();
-
-    if(x + radius > innerWidth || x - radius < 0){
-        dx=-dx;
-    }
-    if(y + radius > innerWidth || y - radius < 0){
-        dy = -dy;
-    }
-
-    x+=dx;
-    y+=dy;
-    
+  for(var i = 0; i< circleArray.length;i++){
+      circleArray[i].update();
+  }
 }
 animate();
 
